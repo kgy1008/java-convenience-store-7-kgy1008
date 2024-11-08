b81a1e4f3ae572065ccfb9cfe42624fab45ee198
kgy1008 <gayeon1008@g.skku.edu>
2024-11-09 01:51:22 +0900

    feat(InputView): ask user to buy without promotion

--- src/main/java/store/io/view/InputView.java
+++ src/main/java/store/io/view/InputView.java
@@ -7,18 +7,25 @@ import store.common.exception.AppException;
 
 public class InputView {
 
+    private static final String NEW_LINE = System.lineSeparator();
     private static final String PURCHASE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String PROMOTION_WARNING_MESSAGE =
+            NEW_LINE + "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)" + NEW_LINE;
     private static final String DELIMITER = ",";
     private static final String PREFIX = "[";
     private static final String SUFFIX = "]";
     private static final String DETAIL_DELIMITER = "-";
-    private static final String NEW_LINE = System.lineSeparator();
 
     public String inputShoppingItems() {
         System.out.println(NEW_LINE + PURCHASE_MESSAGE);
         String input = Console.readLine();
         validate(input);
         return input;
+    }
+
+    public String askForPurchaseWithWarning(final String name, final int quantity) {
+        System.out.printf(PROMOTION_WARNING_MESSAGE, name, quantity);
+        return Console.readLine();
     }
 
     private void validate(final String input) {
