b378c3e5b05de0495255c677c6179d673e5e5923
kgy1008 <gayeon1008@g.skku.edu>
2024-11-09 02:26:20 +0900

    feat: ask add product to get benefit

--- src/main/java/store/io/view/InputView.java
+++ src/main/java/store/io/view/InputView.java
@@ -11,6 +11,8 @@ public class InputView {
     private static final String PURCHASE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String PROMOTION_WARNING_MESSAGE =
             NEW_LINE + "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)" + NEW_LINE;
+    private static final String PROMOTION_BENEFIT_MESSAGE =
+            NEW_LINE + "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)" + NEW_LINE;
     private static final String DELIMITER = ",";
     private static final String PREFIX = "[";
     private static final String SUFFIX = "]";
@@ -25,6 +27,11 @@ public class InputView {
 
     public String askForPurchaseWithWarning(final String name, final int quantity) {
         System.out.printf(PROMOTION_WARNING_MESSAGE, name, quantity);
+        return Console.readLine();
+    }
+
+    public String askForBenefitWithAdditional(final String name) {
+        System.out.printf(PROMOTION_BENEFIT_MESSAGE, name);
         return Console.readLine();
     }
 
