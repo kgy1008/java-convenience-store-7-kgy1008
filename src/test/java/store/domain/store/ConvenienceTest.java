68f6f2f560603646fa3b5569358b61d159a5eb7f
kgy1008 <gayeon1008@g.skku.edu>
2024-11-09 15:22:19 +0900

    test(ConvenienceTest): test for fixed logic

--- src/test/java/store/domain/store/ConvenienceTest.java
+++ src/test/java/store/domain/store/ConvenienceTest.java
@@ -4,22 +4,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import store.domain.user.ShoppingProduct;
 
 class ConvenienceTest {
 
     private final Convenience convenience = new Convenience();
-
-    @Test
-    @DisplayName("프로모션 재고가 부족한 상황 테스트")
-    void checkPromotionRemainingStock() {
-        // given
-        ShoppingProduct shoppingProduct = new ShoppingProduct("콜라", 15);
-        boolean expected = true;
-
-        // when & then
-        assertThat(convenience.isGreaterThanPromotionRemaingStock(shoppingProduct)).isEqualTo(expected);
-    }
 
     @Test
     @DisplayName("프로모션 적용이 가능한 상품에 대해 고객이 해당 수량보다 적게 가져온 상황 테스트")
@@ -32,14 +23,19 @@ class ConvenienceTest {
         assertThat(convenience.canReceiveAdditionalBenefit(shoppingProduct)).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("프로모션 혜택 없이 결제해야 하는 경우, 정가로 결제할 수량 개수 테스트")
-    void checkCountOfProductWithoutPromotion() {
+    @CsvSource({
+            "컵라면, 2, 2",
+            "콜라, 10, 1",
+            "콜라, 11, 2",
+            "콜라, 12, 3"
+    })
+    void checkCountOfProductWithoutPromotion(final String productName, final int productCount, final int expected) {
         // given
-        ShoppingProduct shoppingProduct = new ShoppingProduct("초코바", 7);
-        int expected = 3;
+        ShoppingProduct shoppingProduct = new ShoppingProduct(productName, productCount);
 
-        assertThat(convenience.getItemCountWithoutPromotion(shoppingProduct)).isEqualTo(expected);
+        // when & then
+        assertThat(convenience.calculateItemCountWithoutPromotion(shoppingProduct)).isEqualTo(expected);
     }
-
 }
