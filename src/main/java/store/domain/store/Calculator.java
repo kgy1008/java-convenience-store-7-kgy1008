28ac78b65f270dd0e4087c87e216bcf1eedc778c
kgy1008 <gayeon1008@g.skku.edu>
2024-11-09 18:27:39 +0900

    feat: calculate totalPrice

--- src/main/java/store/domain/store/Calculator.java
+++ src/main/java/store/domain/store/Calculator.java
@@ -3,12 +3,30 @@ package store.domain.store;
 import java.util.ArrayList;
 import java.util.List;
 import store.domain.user.ShoppingProduct;
+import store.dto.Gift;
+import store.dto.Receipt;
 
 public class Calculator {
 
+    private static final int NO_MEMBERSHIP_DISCOUNT = 0;
     private final List<ShoppingProduct> cart;
+    private final List<Gift> gifts;
 
-    public Calculator(final List<ShoppingProduct> shoppingProducts) {
-        cart = new ArrayList<>(shoppingProducts);
+    public Calculator(final List<ShoppingProduct> products, final List<Gift> gifts) {
+        this.cart = new ArrayList<>(products);
+        this.gifts = new ArrayList<>(gifts);
+    }
+
+    public Receipt calculatePrice(final boolean hasMembershipBenefit) {
+        int totalPrice = calculateTotalPrice();
+   
+    }
+
+    private int calculateTotalPrice() {
+        return cart.stream()
+                .mapToInt(product ->
+                        product.getQuantity() * product.getPrice()
+                )
+                .sum();
     }
 }
