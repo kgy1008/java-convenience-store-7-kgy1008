a682a5239e2dc61202e1a98257bc33e61c06cfe5
kgy1008 <gayeon1008@g.skku.edu>
2024-11-09 12:46:47 +0900

    feat(ProductFormatter): throw exception when input quantity is invalid

--- src/main/java/store/domain/store/util/ProductFormatter.java
+++ src/main/java/store/domain/store/util/ProductFormatter.java
@@ -2,6 +2,8 @@ package store.domain.store.util;
 
 import static store.common.ErrorMessage.CONFLICT_EXCEPTION;
 import static store.common.ErrorMessage.EXCEED_QUANTITY;
+import static store.common.ErrorMessage.INVALID_FORMAT;
+import static store.common.ErrorMessage.INVALID_INPUT;
 import static store.common.ErrorMessage.NOT_FOUND;
 
 import java.util.Arrays;
@@ -18,6 +20,7 @@ public class ProductFormatter {
     private static final String PREFIX = "[";
     private static final String SUFFIX = "]";
     private static final String DETAIL_DELIMITER = "-";
+    private static final int MINIMUN_PURCHASE_QUANTITY = 1;
 
     public List<ShoppingProduct> convertStringToItem(final String input, final Items items) {
         List<ShoppingProduct> shoppingProducts = Arrays.stream(input.split(DELIMITER))
@@ -30,9 +33,17 @@ public class ProductFormatter {
     private ShoppingProduct parseData(final String input, final Items items) {
         String[] data = input.replace(PREFIX, "").replace(SUFFIX, "").split(DETAIL_DELIMITER);
         String name = data[0];
-        int quantity = Integer.parseInt(data[1]);
+        int quantity = convertStringToInt(data[1]);
         validate(name, quantity, items);
         return new ShoppingProduct(name, quantity);
+    }
+
+    private int convertStringToInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new AppException(INVALID_FORMAT.getMessage());
+        }
     }
 
     private void validate(final String name, final int quantity, final Items items) {
@@ -47,12 +58,19 @@ public class ProductFormatter {
     }
 
     private void validateQuantity(final String name, final int quantity, final Items items) {
-        if (items.checkRemainingStock(name) < quantity) {
+        if (quantity < MINIMUN_PURCHASE_QUANTITY) {
+            throw new AppException(INVALID_INPUT.getMessage());
+        }
+        if (isExceedQuantity(name, quantity, items)) {
             throw new AppException(EXCEED_QUANTITY.getMessage());
         }
     }
 
-    private void validateNoDuplicate(List<ShoppingProduct> shoppingProducts) {
+    private boolean isExceedQuantity(final String name, final int quantity, final Items items) {
+        return items.checkRemainingStock(name) < quantity;
+    }
+
+    private void validateNoDuplicate(final List<ShoppingProduct> shoppingProducts) {
         Set<ShoppingProduct> productSet = new HashSet<>(shoppingProducts);
         if (productSet.size() != shoppingProducts.size()) {
             throw new AppException(CONFLICT_EXCEPTION.getMessage());
