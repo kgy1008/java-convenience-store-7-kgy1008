41411be9f6655c2e1695e275cd68094a6d4d0123
kgy1008 <gayeon1008@g.skku.edu>
2024-11-10 23:06:29 +0900

    test(ApplicationTest): add test cases

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -1,13 +1,12 @@
 package store;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -15,24 +14,24 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("[물-1]", "N", "N");
             assertThat(output()).contains(
-                "- 콜라 1,000원 10개 탄산2+1",
-                "- 콜라 1,000원 10개",
-                "- 사이다 1,000원 8개 탄산2+1",
-                "- 사이다 1,000원 7개",
-                "- 오렌지주스 1,800원 9개 MD추천상품",
-                "- 오렌지주스 1,800원 재고 없음",
-                "- 탄산수 1,200원 5개 탄산2+1",
-                "- 탄산수 1,200원 재고 없음",
-                "- 물 500원 10개",
-                "- 비타민워터 1,500원 6개",
-                "- 감자칩 1,500원 5개 반짝할인",
-                "- 감자칩 1,500원 5개",
-                "- 초코바 1,200원 5개 MD추천상품",
-                "- 초코바 1,200원 5개",
-                "- 에너지바 2,000원 5개",
-                "- 정식도시락 6,400원 8개",
-                "- 컵라면 1,700원 1개 MD추천상품",
-                "- 컵라면 1,700원 10개"
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 8개 탄산2+1",
+                    "- 사이다 1,000원 7개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 10개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개"
             );
         });
     }
@@ -58,6 +57,26 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             runException("[컵라면-12]", "N", "N");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 프로모션_혜택을_받지_못하는_상품_안내메세지_출력() {
+        assertSimpleTest(() -> {
+            run("[콜라-11]", "N", "N", "N");
+            assertThat(output()).contains(
+                    "현재 콜라 2개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"
+            );
+        });
+    }
+
+    @Test
+    void 프로모션_혜택을_받을_수_있는_상품_안내메세지_출력() {
+        assertSimpleTest(() -> {
+            run("[콜라-2]", "N", "N", "N");
+            assertThat(output()).contains(
+                    "현재 콜라은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
+            );
         });
     }
 
