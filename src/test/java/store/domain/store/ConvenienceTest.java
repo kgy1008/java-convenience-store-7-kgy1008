27d522983f085e4844259f1115644e370bb30bbb
kgy1008 <gayeon1008@g.skku.edu>
2024-11-10 19:56:56 +0900

    test(ConvenienceTest): test for update items stock

--- src/test/java/store/domain/store/ConvenienceTest.java
+++ src/test/java/store/domain/store/ConvenienceTest.java
@@ -2,10 +2,14 @@ package store.domain.store;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import store.domain.store.item.BasicItem;
+import store.domain.store.item.Item;
+import store.domain.store.item.Items;
 import store.domain.store.item.PromotionItem;
 
 class ConvenienceTest {
@@ -36,5 +40,45 @@ class ConvenienceTest {
         PromotionItem promotionItem = new PromotionItem(productName, price, productCount, promotion);
         // when & then
         assertThat(convenience.calculateItemCountWithoutPromotion(promotionItem)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("고객의 쇼핑 목록 중 프로모션 혜택으로 받을 수 있는 증정품 개수 테스트")
+    void checkCountOfFreeItemWithPromotion() {
+        // given
+        PromotionItem promotionItem = new PromotionItem("콜라", 1000, 9, "탄산2+1");
+        // when
+        int result = convenience.calculateNumberOfFreeItem(promotionItem);
+        int expected = 3;
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("고객이 결제 후, 그 수량만큼 재고 차감 테스트")
+    void updateStockAfterPayment() {
+        // given
+        Items items = convenience.getItems();
+        List<PromotionItem> promotionItems = List.of(
+                new PromotionItem("콜라", 1000, 15, "탄산2+1"),
+                new PromotionItem("오렌지주스", 1800, 5, "MD추천상품")
+        );
+        List<BasicItem> basicItems = List.of(
+                new BasicItem("에너지바", 2000, 3)
+        );
+        // when
+        convenience.updateItemQuantity(promotionItems, basicItems);
+        // then
+        List<Item> cokeItem = items.findItemsByName("콜라");
+        Item promotionCoke = cokeItem.getFirst();
+        Item basicCoke = cokeItem.getLast();
+        assertThat(promotionCoke.getQuantity()).isZero();
+        assertThat(basicCoke.getQuantity()).isEqualTo(5);
+
+        Item promotionOrangeJuice = items.findItemByName("오렌지주스");
+        assertThat(promotionOrangeJuice.getQuantity()).isEqualTo(4);
+
+        Item BasicEnergyStick = items.findItemByName("에너지바");
+        assertThat(BasicEnergyStick.getQuantity()).isEqualTo(2);
     }
 }
