8dd983728f881be780ad5d8045a756400842683a
kgy1008 <gayeon1008@g.skku.edu>
2024-11-11 22:32:34 +0900

    refactor(BasicItem): move repeated logic to @BeforeEach

--- src/test/java/store/domain/store/item/BasicItemTest.java
+++ src/test/java/store/domain/store/item/BasicItemTest.java
@@ -3,16 +3,22 @@ package store.domain.store.item;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BasicItemTest {
 
+    private BasicItem basicItem;
+
+    @BeforeEach
+    void setUp() {
+        basicItem = new BasicItem("물", 500, 10);
+    }
+
     @Test
     @DisplayName("재고 차감 테스트")
     void decreaseQuantity() {
-        // given
-        BasicItem basicItem = new BasicItem("물", 500, 10);
         // when
         basicItem.decreaseQuantity(5);
         // then
@@ -22,18 +28,14 @@ class BasicItemTest {
     @Test
     @DisplayName("재고가 0개 미만일때, 예외 발생")
     void invalidQuantity() {
-        // given
-        BasicItem basicItem = new BasicItem("물", 500, 2);
         // when & then
-        assertThatThrownBy(() -> basicItem.decreaseQuantity(5))
+        assertThatThrownBy(() -> basicItem.decreaseQuantity(11))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("재고 증가 테스트")
     void increaseQuantity() {
-        // given
-        BasicItem basicItem = new BasicItem("물", 500, 10);
         // when
         basicItem.increaseQuantity(1);
         // then
